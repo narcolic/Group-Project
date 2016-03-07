66d0c305e771c241ec0be01d8323213266e4da0b
Dimitrios Karakatsanis <karakatd@aston.ac.uk>
2016-03-07 10:56:40 +0000

    Needs default position coords.

--- src/Game/Pawn.java
+++ src/Game/Pawn.java
@@ -5,38 +5,62 @@ import java.awt.Color;
 
 public class Pawn {
 
-	public int pawnID;
-	private Position position;
-	
-	/**
-	 * Constructor
-	 */
-	public Pawn(){
-		
-	}
-	
-	/**
-	 * 
-	 * @param c
-	 */
-	public void assignColour(Color c){
-		
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public Position getPosition(){
-		return null;
-	}
-	
-	/**
-	 * 
-	 * @param direction
-	 */
-	public void move(int direction){
-		
-	}
-	
+    private int pawnID;
+    private Position position;
+    private Color pawnColor;
+
+    /**
+     * Constructor
+     */
+    public Pawn(int newID) {
+        pawnID = newID;
+        setDefaultPosition();
+        setDefaultColor();
+    }
+
+    public void updateCurrentPosition(Position newPosition) {
+        this.position = newPosition;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+/*    public void setNewPawnID(int newID) {
+        this.pawnID = newID;
+    }*/
+
+    public void setDefaultPosition() {
+        switch (pawnID) {
+            case 1:
+                this.position = null;
+                break;
+            case 2:
+                this.position = null;
+                break;
+            case 3:
+                this.position = null;
+                break;
+            case 4:
+                this.position = null;
+                break;
+        }
+    }
+
+    public void setDefaultColor() {
+        switch (pawnID) {
+            case 1:
+                this.pawnColor = Color.RED;
+                break;
+            case 2:
+                this.pawnColor = Color.BLUE;
+                break;
+            case 3:
+                this.pawnColor = Color.GREEN;
+                break;
+            case 4:
+                this.pawnColor = Color.YELLOW;
+                break;
+        }
+    }
 }
