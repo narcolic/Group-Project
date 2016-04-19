089f308a80abdc9046272f5c5143d57d211d78d4
Flash Kirby <flashkirby@gmail.com>
2016-04-19 03:13:45 +0100

    Fixed and implemented Fence methods, and added additional methods to Orientation for ease of coding. Fixed Position returning 0 on getX amd getY; now returns expected values.

--- src/Game/Orientation.java
+++ src/Game/Orientation.java
@@ -1,3 +1,5 @@
+package Game;
+
 public class Orientation {
 	public enum Direction{
 		NORTH, EAST, SOUTH, WEST;
@@ -21,5 +23,36 @@ public class Orientation {
 		public Direction getOppositeDirection(){
 			return opposite;
 		}
+	}
+	
+	/**
+	 * Categorises NORTH/SOUTH and WEST/EAST as a boolean.
+	 * @param direction
+	 * @return true if NORTH/SOUTH
+	 */
+	public static boolean isVertical(Direction direction)
+	{
+		if (direction == Direction.NORTH || direction == Direction.SOUTH) return true;
+		return false;
+	}
+	/**
+	 * Compares two directions to see if they are of the same directions.
+	 * @param direction1
+	 * @param direction2
+	 * @return
+	 */
+	public static boolean isParallel(Direction direction1, Direction direction2)
+	{
+		return isVertical(direction1) == isVertical(direction2);
+	}
+	/**
+	 * Compares a direction and a preset boolean.
+	 * @param direction
+	 * @param isVertical
+	 * @return
+	 */
+	public static boolean isParallel(Direction direction, boolean isVertical)
+	{
+		return isVertical(direction) == isVertical;
 	}
 }
