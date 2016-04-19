b04df1d3d98ff45ab475152d3c36b7bd7d3cbd3d
Daniel Edmondson <Daniel@109.175.198.51>
2016-04-19 14:43:39 +0100

    Changed to javaFX

--- src/Menu/MainMenu.java
+++ src/Menu/MainMenu.java
@@ -1,59 +1,92 @@
 package Menu;
 
-import javax.swing.*;
-import java.awt.*;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
 
-public class MainMenu {
-
+public class MainMenu extends Application {
     public static void main(String[] args) {
-        new MainMenu();
+        launch(args);
     }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Main Menu");
 
-    public MainMenu() {
-        EventQueue.invokeLater(new Runnable() {
+        //create the start button
+        Button start = new Button();
+        start.setText("Start");
+        start.setMaxWidth(Double.MAX_VALUE);
+        start.setOnAction(new EventHandler<ActionEvent>() {
+        	
             @Override
-            public void run() {
-                try {
-                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                } catch (ClassNotFoundException ex) {
-                } catch (InstantiationException ex) {
-                } catch (IllegalAccessException ex) {
-                } catch (UnsupportedLookAndFeelException ex) {
-                }
-
-                JFrame frame = new JFrame();
-                frame.setTitle("Quoridor");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setLayout(new BorderLayout());
-                frame.add(new MenuPanel());
-                frame.pack();
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
+            public void handle(ActionEvent event) {
+                System.out.println("Start");
             }
         });
-    }
+        
+        
+        //create the options button
+        Button options = new Button();
+        options.setText("Options");
+        options.setMaxWidth(Double.MAX_VALUE);
+        options.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Options");
+            }
+        });
+        
+        //create the Help button
+        Button help = new Button();
+        help.setText("Help");
+        help.setMaxWidth(Double.MAX_VALUE);
+        help.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Help");
+            }
+        });
+        
+        
+        //create the Quit button
+        Button quit = new Button();
+        quit.setText("Quit");
+        quit.setMaxWidth(Double.MAX_VALUE);
+        quit.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Quit");
+                primaryStage.close()  ;      
+                }
+        });
+        
+        //set order of buttons
+        GridPane root = new GridPane();
+        GridPane.setRowIndex(start,1);
+        GridPane.setRowIndex(options,2);
+        GridPane.setRowIndex(help,3);
+        GridPane.setRowIndex(quit,4);
 
-    protected class MenuPanel extends JPanel {
-
-        public MenuPanel() {
-           // JLabel label = new JLabel("Quoridor");
-            setLayout(new GridBagLayout());
-            setPreferredSize(new Dimension(800,600));
-            setMinimumSize(getPreferredSize());
-            
-            GridBagConstraints constraints = new GridBagConstraints();
-            constraints.gridx = 0;
-            constraints.gridy = 0;
-           // add(label, constraints);
-
-            constraints.fill = GridBagConstraints.HORIZONTAL;
-            constraints.gridy++;
-            add(new JButton("Start Game"), constraints);
-            constraints.gridy++;
-            
-            add(new JButton("Options"), constraints);
-            constraints.gridy++;
-            add(new JButton("Quit"), constraints);
-        }
+        //add buttons to menu
+        root.getChildren().add(start);
+        root.getChildren().add(options);
+        root.getChildren().add(help);
+        root.getChildren().add(quit);
+        root.setAlignment(Pos.CENTER);
+        
+        
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }
