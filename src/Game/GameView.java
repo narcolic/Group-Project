dd9f14ec92706d615d813c94766e4b9201ddc50b
edmondd1 <edmondd1@11068MBE269.campus.aston.ac.uk>
2016-04-21 12:08:49 +0100

    swing

--- src/Game/GameView.java
+++ src/Game/GameView.java
@@ -1,56 +1,43 @@
 package Game;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
-import javafx.scene.input.MouseEvent;
+import java.awt.Graphics;
+import java.awt.Color;
 
+import javax.swing.JComponent;
+import javax.swing.JFrame;
 
-public class GameView extends Application {
-    public static void main(String[] args) {
-        Application.launch(args);
-    }
-        
-    @Override
-    public void start(Stage gameStage) {
-    	gameStage.setTitle("Quoridor");
-        Group root = new Group();
-        Scene scene = new Scene(root, 600, 600, Color.WHITE);
-        
-        int x = 0; //X coordinate of tile
-        int y = 0; //Y coordinate of tile
-        int posX = 50; //Position of tile in scene
-        int posY = 50; //Position of tile in scene
-       
-        //loop to create a grid of 9x9 tiles
-        while(y < 9){
-        	Rectangle grid = new Rectangle();
-        	grid.setX(posX);
-        	grid.setY(posY);
-        	grid.setWidth(50);
-        	grid.setHeight(50);
-        	grid.setFill(Color.RED);
-        	
-        	root.getChildren().add(grid); 
-        	x++;
-        	posX = posX+60;
-        	
-        	//if there are 9 tiles in row, go to next row
-        	if(x == 9){
-        		posX = 50;
-        		posY = posY +60;
-        		x = 0;
-        		y++;
-        		
-        	}
-        }
+class MyCanvas extends JComponent {
 
-        gameStage.setScene(scene);
-        gameStage.show();
-    }
+	public void paint(Graphics g) {
+		int rowNum = 1;
+		int colNum = 1;
+		int posX = 50;
+		int posY = 50;
+
+		while (rowNum <= 9) {
+
+			g.setColor(Color.red);
+			g.fillRect(posX, posY, 50, 50);
+			colNum++;
+			posX = posX + 60;
+
+			if (colNum == 10) {
+				colNum = 1;
+				rowNum++;
+				posY = posY + 60;
+				posX = 50;
+
+			}
+		}
+	}
+}
+
+public class GameView {
+	public static void main(String[] a) {
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setBounds(30, 30, 800, 800);
+		window.getContentPane().add(new MyCanvas());
+		window.setVisible(true);
+	}
 }
