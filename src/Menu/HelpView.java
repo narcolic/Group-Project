4efb968bb832ee4bbf5c977afcce2fb065588bb7
Farah-N <farah.n@hotmail.co.uk>
2016-04-21 11:43:25 +0100

    Update HelpView.java

--- src/Menu/HelpView.java
+++ src/Menu/HelpView.java
@@ -1,9 +1,13 @@
 package Menu;
 
 
+import java.util.ArrayList;
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
@@ -14,13 +18,22 @@ public class HelpView extends Application{
 	public static void main(String[] args) {
         launch(args);
     }
+	
+	public Button next;
+	public Button previous;
+	
+	private String[] textHelp;
 	Image[] img;
+	
+	TextArea text = new TextArea();
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		GridPane root = new GridPane();
 		img = new Image[5];
 		img[0]=new Image("/Images/dog.jpg");
+		
+		
 		//img[1]=new Image("/Images/cat.png");
 		
 	    //Image image = new Image("/Images/dog.jpg"); // image to be used
@@ -32,9 +45,25 @@ public class HelpView extends Application{
 	   
 	    //	    root.getChildren().add(imageView);
 
+	    text.appendText(textHelp[0]);
+	    
+	    next = new Button();
+	    next.setText("Next");
+	    
+	    previous = new Button();
+	    previous.setText("Previous");
+	    
 	    root.getChildren().add(new ImageView(img[0]));
 	    root.setAlignment(Pos.CENTER);
 	    
+	    root.getChildren().add(next);
+	    root.setAlignment(Pos.CENTER_RIGHT);
+	    
+	    root.getChildren().add(previous);
+	    root.setAlignment(Pos.CENTER_LEFT);
+	    
+	    root.getChildren().add(text);
+	    root.setAlignment(Pos.BOTTOM_CENTER);
 	    primaryStage.setScene(new Scene(root, 300, 250));
 	    primaryStage.show();
 	}
@@ -65,3 +94,4 @@ public class HelpView extends Application{
 	}*/
 	
 }
+
