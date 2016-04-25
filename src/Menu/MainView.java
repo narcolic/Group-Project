9dc1a6a84d019e32f676ce87d7279dfbaad20fe7
Dimitrios Karakatsanis <karakatd@aston.ac.uk>
2016-04-25 20:36:45 +0100

    MainView code rework Css Style Code Cleaning

--- src/Menu/MainView.java
+++ src/Menu/MainView.java
@@ -9,7 +9,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.AudioClip;
 import javafx.scene.paint.Color;
