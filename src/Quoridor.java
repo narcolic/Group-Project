55770bca15b60b2b888543642b862cf829fb3d11
Dimitrios Karakatsanis <karakatd@aston.ac.uk>
2016-04-27 19:21:20 +0100

    CCU

--- src/Quoridor.java
+++ src/Quoridor.java
@@ -1,11 +1,9 @@
-import Menu.MainView;
 import Menu.MenuController;
 
 public class Quoridor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MainView menuView = new MainView();
 		MenuController menuController = new MenuController();
 		
 	}
